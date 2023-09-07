92bf96cb29711df20f1cbf7d343ad34c8a208518
John Blalock <simpledimplejohn@gmail.com>
2023-09-07 08:31:07 -0400

    form script basic working

---
+++ public/scripts.js
@@ -0,0 +1,23 @@
+// getting from the form
+console.log("scripts connects")
+const form = document.getElementById("userForm");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("form submits")
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const birthdate = document.getElementById("birthdate").value;
+    const birthTime = document.getElementById("birthTime").value;
+    const birthPlace = document.getElementById("firstName").value;
+
+    console.log(firstName)
+    let element = document.getElementById("output");
+    element.textContent = `User Info Is  \n
+    ${firstName}`
+})
+
+
+
+
+
