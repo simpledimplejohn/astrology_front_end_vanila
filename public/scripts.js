34d0fe9a8f4cdafa43bf49c53f2784566f42e13e
John Blalock <simpledimplejohn@gmail.com>
2023-09-07 17:30:40 -0400

    form now populates in the user info section

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,32 @@
-// getting from the form
 console.log("scripts connects")
 const form = document.getElementById("userForm");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("form submits")
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
     const birthdate = document.getElementById("birthdate").value;
     const birthTime = document.getElementById("birthTime").value;
-    const birthPlace = document.getElementById("firstName").value;
+    const latitude = document.getElementById("latitude").value;
+    const longitude = document.getElementById("longitude").value;
 
-    console.log(firstName)
     let element = document.getElementById("output");
-    element.textContent = `User Info Is  \n
-    ${firstName}`
+    
+    
+
+
+    let userInfo = `User Info Is:<br>
+    First Name: ${firstName}<br>
+    Last Name: ${lastName}<br>
+    Birthdate: ${birthdate}<br>
+    Birth Time: ${birthTime}<br>
+    Latitude: ${latitude}<br>
+    Longitude: ${longitude}`;
+    
+    // Set the "output" element's text content
+    element.innerHTML = userInfo;
 });
+
 
 
 
